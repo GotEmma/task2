a4043a1597520320acd57d630a6ec10e46100da8
Linnea Svensson <svensson.47@hotmail.com>
2017-02-12 13:58:42 +0100

    first script

---
+++ task2/task2.sql
@@ -0,0 +1,42 @@
+CREATE TABLE Countries (name TEXT,
+  PRIMARY KEY (name)
+);
+CREATE TABLE Areas (country TEXT,
+  name TEXT,
+  population INT,
+  PRIMARY KEY(country, name),
+  FOREIGN KEY (country) REFERENCES Countries (name),
+  CONSTRAINT pop_positive CHECK (population >= 0)
+);
+CREATE TABLE Towns(country TEXT,
+  name TEXT,
+  PRIMARY KEY(country, name),
+  FOREIGN KEY(country, name) REFERENCES Areas (country, name)
+);
+CREATE TABLE Cities (country TEXT,
+  name TEXT,
+  visitbonus INT,
+  PRIMARY KEY(country, name),
+  FOREIGN KEY(country, name) REFERENCES Areas(country, name)
+);
+CREATE TABLE Persons(country TEXT,
+  personnummer VARCHAR(13),
+  name TEXT,
+  locationcountry TEXT,
+  locationarea TEXT,
+  budget NUMERIC,
+  PRIMARY KEY(country, personnummer),
+  FOREIGN KEY(country) REFERENCES Countries(name),
+  FOREIGN KEY(locationcountry, locationarea) REFERENCES Areas(country, name),
+  CONSTRAINT format_personnummer CHECK
+  (personnummer LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]' )
+);
+CREATE TABLE Hotels (name TEXT,
+  locationcountry TEXT,
+  locationname TEXT,
+  ownercountry TEXT,
+  ownerpersonnummer VARCHAR(13),
+  PRIMARY KEY(locationcountry, locationname, ownercountry, ownerpersonnummer),
+  FOREIGN KEY(locationcountry, locationname) REFERENCES Cities(country, name),
+  FOREIGN KEY(ownercountry, ownerpersonnummer) REFERENCES Persons(country, personnummer)
+);
