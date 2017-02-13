e710088cf6e4cc1dde3393fe013d314ba58c1f31
Linnea Svensson <svensson.47@hotmail.com>
2017-02-13 19:14:02 +0100

    added a test for road

---
+++ shouldfail/testroads1.sql
@@ -0,0 +1,5 @@
+--tests that the fromarea and toarea can't be identical
+INSERT INTO Countries VALUES ('Sweden');
+INSERT INTO Areas VALUES ('Sweden','Gothenburg',491630);
+INSERT INTO Persons VALUES ('Sweden','12345678-1234','player1','Sweden','Gothenburg',1000);
+INSERT INTO Roads VALUES ('Sweden','Gothenburg','Sweden','Gothenburg','Sweden','12345678-1234',90);
